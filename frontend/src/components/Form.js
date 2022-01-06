ca68f2bb145c1b355e3ba9da66343e9a2567c3f5
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2022-01-06 14:39:39 +0100

    Set required fields

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -6,11 +6,26 @@ import { useState } from 'react';
 
 const Form = () => {
 
+    function sendMail(e){
+        e.preventDefault();
+        emailjs.sendForm('service_4m5j5lq', 'template_prmn5f4', e.target, 'user_PoozJwyLbxQ1CnVKbB7bs'
+    ).then(res =>{
+        console.log(res);
+        alert('Bericht werd succesvol verzonden!');
+        setValues({
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+        });
+    }).catch(err => console.log(err))
+}
+    
     const [values, setValues] = useState({
-        naam: '',
+        name: '',
         email: '',
-        onderwerp: '',
-        bericht:''
+        subject: '',
+        message:''
     });
     console.log(values);
 
@@ -20,42 +35,34 @@ const Form = () => {
             [e.target.name]: e.target.value
         }))
     }
-
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-
-        emailjs.send('service_4m5j5lq', 'template_prmn5f4', values, 'user_PoozJwyLbxQ1CnVKbB7bs')
-        .then(response =>{
-            console.log('Succes!', response);
-            setValues({
-                naam: '',
-                email: '',
-                onderwerp: '',
-                bericht: '',
-            });
-            alert('Bericht werd succesvol verzonden!');
-        }, error =>{
-            console.log('Failed!', error);
-        })
-    }
-    
     return (
         <div>
         <h1 className="page-title">Formulier</h1>
         <div className="container">
-            <form onSubmit={handleSubmit} style={{margin: 0, padding: 0, display: 'flex', justifyContent: 'center'}}>
+            <form onSubmit={sendMail} style={{margin: 0, padding: 0, display: 'flex', justifyContent: 'center'}}>
                 <div className="row pt-5 mx-auto">
                     <div className="col-8 form-group mx-auto">
-                        <input type="text" className="form-control" placeholder="Naam" name="naam"/>
+                    <label>name</label> 
+                        <input type="text" className="form-control" placeholder="Naam" name="name"
+                            required
+                        />
                     </div>
                     <div className="col-8 form-group pt-2 mx-auto">
-                            <input handleChange={handleChange} type="email" className="form-control" placeholder="Email" name="email"/>
+                    <label>email</label> 
+                            <input handleChange={handleChange} type="email" className="form-control" placeholder="Email" name="email"
+                                required
+                            />
                         </div>
                         <div className="col-8 form-group pt-2 mx-auto">
-                            <input handleChange={handleChange} type="text" className="form-control" placeholder="Onderwerp" name="onderwerp"/>
+                        <label>onderwerp</label> 
+                            <input handleChange={handleChange} type="text" className="form-control" placeholder="Onderwerp" name="subject"
+                                required
+                            />
                         </div>
                         <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea handleChange={handleChange} className="form-control" id="" cols="30" rows="8" placeholder="Uw bericht" name="bericht"></textarea>
+                        <label>bericht</label> 
+                            <textarea handleChange={handleChange} className="form-control" id="" cols="30" rows="8" placeholder="Uw bericht" name="message"
+                            required></textarea>
                         </div>
                         <div className="col-8 pt-3 mx-auto">
                             <input type="submit" className="btn-btn-info" value="Verstuur!"/>
